5282c52d440faf069a610336ad09105ed1c357fa
lkaruppiah54462 <lakshmazda@gmail.com>
2020-07-04 17:37:09 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # surfs_up
+
+##A. Background
+
+Extracted temperature and precipitation data for island of **OHAHU** 
+from 2010-01-01 to 2017-08-23.
+Analyzed by month (June and Decmber) to see the  weather pattern and suitability 
+of the weather for surfing/snack shop.
+
+##B. Results
+
+JUNE
+	precipitation	temperature
+count	1574.000000	1700.000000
+mean	0.136360	74.944118
+std	0.335731	3.257417
+min	0.000000	64.000000
+25%	0.000000	73.000000
+50%	0.020000	75.000000
+75%	0.120000	77.000000
+max	4.430000	85.000000
+
+DECEMBER
+	precipitation	temperature
+count	1405.000000	1517.000000
+mean	0.216819	71.041529
+std	0.541399	3.745920
+min	0.000000	56.000000
+25%	0.000000	69.000000
+50%	0.030000	71.000000
+75%	0.150000	74.000000
+max	6.420000	83.000000
+
+##C. Observation
+
+1. From the tables above december is wetter and cooler 
+in the island of OHAHU compared to the month of June
+2. Standard deviation for temperature is a lot smaller , 
+indicating the temperature flucutuations are smaller. 
+Seems the range is about 11 deg F for each month
+3. Precipitation is almost 60% higher in decmber compared
+to June gthroughout the 7 years.
+4. Variation in precipaitation for each month is a lot, 
+especially for both months june and december  have somedays 
+that are very wet but from the quartiles, the number of days 
+is probably small. i.e the number of days the business could be slow.
+
+
+##D. Recommendation for further Analysis
+
+1. Look at data by station to see if there are discrpency due to some stations 
+reporting erroneous data, filter them out if necessary
+
+2. Analyze by station to see if there are locations in OHAHU
+that is better to set up shop
+
+3. Analyze data for the school holiday months, e,g spring break in march,
+summer months June,July and August, december break December and in particular 
+the last two weeks in december/1 week in January.
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask,jsonify
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -6,8 +5,9 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-engine = create_engine("sqlite:///hawaii.sqlite")
+from flask import Flask,jsonify
+#if check same thread not there, second querry fails
+engine = create_engine("sqlite:///hawaii.sqlite",connect_args={'check_same_thread': False})
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
@@ -16,6 +16,7 @@ session = Session(engine)
 
 #FLASK APPLICATION
 #Define routes
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -28,14 +29,14 @@ def welcome():
     /api/v1.0/stations
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
+    /api/v1.0/temp/June
+    /api/v1.0/temp/December
     ''')
-
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-	filter(Measurement.date >= prev_year).all()
+   precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
 
@@ -56,15 +57,45 @@ def temp_monthly():
 
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
-
 def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    seltemp = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    selprecp = [func.min(Measurement.prcp), func.avg(Measurement.prcp), func.max(Measurement.prcp)]
     if not end:
-        results = session.query(*sel).filter(Measurement.date <= start).all()
-        temps = list(np.ravel(results))
-        return jsonify(temps)
+        resulttemp = session.query(*seltemp).filter(Measurement.station != 'USC00518838' and Measurement.station != 'USC00517948' ).filter(Measurement.date <= start).all()
+        resultprecp = session.query(*selprecp).filter(Measurement.station != 'USC00518838' and Measurement.station != 'USC00517948').filter(Measurement.date <= start).all() 
+        temps = list(np.ravel(resulttemp))
+        precp = list(np.ravel(resultprecp))
+        return jsonify(temps=temps,precp=precp)    
+    resulttemp = session.query(*seltemp).filter(Measurement.station != 'USC00518838' and Measurement.station != 'USC00517948').filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    resultprecp = session.query(*selprecp).filter(Measurement.station != 'USC00518838' and Measurement.station != 'USC00517948').filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    temps = list(np.ravel(resulttemp))
+    precp = list(np.ravel(resultprecp))
+    return jsonify(temps=temps,precp=precp)
+
+@app.route("/api/v1.0/temp/June")
+def june():
+    results = []
+    m=6 #June
+    results = session.query(Measurement.date, Measurement.prcp,Measurement.tobs).all()
+    # Save the query results as a Pandas DataFrame and set the index to the date column
+    df = pd.DataFrame(results, columns=['date','precipitation','temperature'])
+    df['date'] = pd.to_datetime(df['date'])
+    #print(df.head())
+    df1 = df.loc[(df['date'].dt.month==m)]
     
-    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)
+    return jsonify(df1.describe().to_json())
+
+@app.route("/api/v1.0/temp/December")
+def december():
+    results = []
+    m=12 #June
+    results = session.query(Measurement.date, Measurement.prcp,Measurement.tobs).all()
+    # Save the query results as a Pandas DataFrame and set the index to the date column
+    df = pd.DataFrame(results, columns=['date','precipitation','temperature'])
+    df['date'] = pd.to_datetime(df['date'])
+    #print(df.head())
+    df1 = df.loc[(df['date'].dt.month==m)]
+    
+    return jsonify(df1.describe().to_json())
+ 
  
